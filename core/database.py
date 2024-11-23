2802025b421201080db56deb0f6cd5db2ed54eed
faisy11 <mohamedfayas116@gmail.com>
2024-11-23 12:48:00 +0530

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,19 +1,12 @@
-import pymysql
-from contextlib import contextmanager
+import mysql.connector
 from core.config import settings
 
-@contextmanager
 def db_connection():
-    connection = None
-    try:
-        connection = pymysql.connect(
-            host=settings.MYSQL_HOST,
-            user=settings.MYSQL_USERNAME,
-            password=settings.MYSQL_PASSWORD,
-            database=settings.MYSQL_DB,
-            port=settings.MYSQL_PORT
-        )
-        yield connection
-    finally:
-        if connection:
-            connection.close()
+    connection = mysql.connector.connect(
+        user=settings.MYSQL_USERNAME,
+        password=settings.MYSQL_PASSWORD,
+        host=settings.MYSQL_HOST,
+        database=settings.MYSQL_DB,
+        port=settings.MYSQL_PORT
+    )
+    return connection
